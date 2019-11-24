ea34c8f53d01cd9ac6bac87bad3ca97a53caace4
juliasilva00 <juliacajuru@gmail.com>
2019-11-24 12:56:57 -0300

    errors category, update category

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -5,6 +5,7 @@ import { switchMap } from 'rxjs/operators';
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import toastr from 'toastr';
 
 @Component({
   selector: 'app-category-form',
@@ -16,7 +17,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   currentAction: string;
   categoryForm: FormGroup;
   pageTitle: string;
-  serverErrorsMessages: string[] = null;
+  serverErrorMessages: string[] = null;
   // tslint:disable-next-line:no-inferrable-types
   submittingForm: boolean = false;
   category: Category = new Category();
@@ -36,6 +37,12 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   }
   ngAfterContentChecked() {
     this.setPageTitle();
+  }
+
+  submitForm() {
+    console.log(this.currentAction);
+    this.submittingForm = true;
+    this.currentAction === 'new' ? this.createCategory() : this.updateCategory();
   }
 
   private setCurrentAction() {
@@ -64,6 +71,44 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   }
   private setPageTitle() {
     this.pageTitle = this.currentAction === 'new' ? 'Cadastro de nova Categoria' : `Editando a Categoria ${this.category.name || ''}`;
+  }
+
+  private createCategory() {
+    // tslint:disable-next-line:new-parens
+    const category: Category = Object.assign(new Category, this.categoryForm.value);
+    this.categoryService.create(category)
+      .subscribe(
+        // tslint:disable-next-line:no-shadowed-variable
+        (category) => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      );
+  }
+  private updateCategory() {
+    // tslint:disable-next-line:new-parens
+    const category: Category = Object.assign(new Category, this.categoryForm.value);
+    this.categoryService.update(category)
+    .subscribe(
+      // tslint:disable-next-line:no-shadowed-variable
+      (category) => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    );
+
+  }
+  private actionsForSuccess(category: Category) {
+    toastr.success('Solicitação processada com sucesso!');
+    this.router.navigateByUrl('categories', { skipLocationChange: true }).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    );
+  }
+  private actionsForError(error) {
+    toastr.error('Erro ao processar sua solicitação!');
+    this.submittingForm = false;
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor tente mais tarde'];
+
+    }
   }
 
 }
